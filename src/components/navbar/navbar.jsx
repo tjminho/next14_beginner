fd575946c5965159896285b44a77864b0359aea8
minho <tjminho@gmail.com>
2024-04-16 14:58:16 +0900

    202404161458

---
+++ src/components/navbar/navbar.jsx
@@ -0,0 +1,16 @@
+import styles from './navbar.module.css'
+import Links from './links/links'
+
+const Navbar = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.logo}>Logo</div>
+      <div>
+        <Links />
+      </div>
+     
+      </div>
+  )
+}
+
+export default Navbar
